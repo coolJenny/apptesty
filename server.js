7141297d06294cd2adb9fb0004578897d8f6e2ce
coolJenny <31550194+coolJenny@users.noreply.github.com>
2017-09-27 08:34:00 -0400

    late

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 // server.js
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+
 // If an incoming request uses
 // a protocol other than HTTPS,
 // redirect that request to the
@@ -20,7 +29,6 @@ const forceSSL = function() {
 // middleware
 app.use(forceSSL());
 
-const path = require('path');
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
